99454de96b49d5997b18b79a2c0248fbaef6884a
Mo2men7 <momen.helmy.m@gmail.com>
2026-03-05 06:46:28 +0200

    Completing dashboard page ui

--- src/features/dashboard/views/dashboard-view.tsx
+++ src/features/dashboard/views/dashboard-view.tsx
@@ -2,6 +2,7 @@ import { DashboardHeader } from "../components/dashboard-header";
 import { HeroPatter } from "../components/hero-pattern";
 import { PageHeader } from "@/components/page-header";
 import { TextInputPanel } from "../components/text-input-panel";
+import { QuickActionsPanel } from "../components/quick-actions-panel";
 
 export function DashboardView() {
   return (
@@ -12,6 +13,7 @@ export function DashboardView() {
       <div className="relative space-y-8 p-4 lg:p-16">
         <DashboardHeader />
         <TextInputPanel />
+        <QuickActionsPanel />
       </div>
     </div>
   );
